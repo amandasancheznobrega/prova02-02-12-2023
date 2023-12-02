434d73c189971e2cd9c6e455ec3fcbacfafbd63f
amandasancheznobrega <152558570+amandasancheznobrega@users.noreply.github.com>
2023-12-02 14:45:22 +0000

    entrega

---
+++ banco/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE games;
+
+USE games;
+
+
+CREATE TABLE categorias(id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(255) NOT NULL
+);
+
+SHOW FIElDS FROM categorias;
+
+CREATE TABLE jogos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(255) NOT NULL,
+  id_categorias INT NOT NULL,
+  competitivo BOOLEAN NOT NULL,
+  FOREIGN KEY (id_categorias) REFERENCES categorias(id)
+);
+SHOW FIElDS FROM jogos;
+
+
+CREATE TABLE Plataformas (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255)
+);
+
+
+
+SHOW FIElDS FROM Plataformas;
+
+
+
+
